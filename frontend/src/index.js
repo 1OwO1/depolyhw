f8603b3dc38392d6a201b4eca4de14905b0b41ae
powotato <0225natasha@gmail.com>
2022-12-26 16:52:37 +0800

    ee

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./Containers/App";
 import "./index.css";
-import "./Containers/App.js";
 import reportWebVitals from "./reportWebVitals";
 import { ScoreCardProvider } from "./hooks/useScoreCard";
 
