48ef615c9adaa1e02f894bd1235fa84aa783aa2f
powotato <0225natasha@gmail.com>
2022-12-28 14:13:03 +0800

    ee

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,20 +6,18 @@ import routes from "./routes/scoreCard";
 import path from "path";
 const app = express();
 if (process.env.NODE_ENV === "development") {
-  console.log("jojiosdf");
   app.use(cors());
 }
 
 app.use(express.json());
-app.use("/api", routes);
+app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
-  console.log("jojiosdf");
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  //   app.get("/*", function (req, res) {
-  //     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  //   });
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
 }
 
 db.connect();
