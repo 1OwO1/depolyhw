d91d1cf012542f63ddd1f5a54751d573c207861f
powotato <0225natasha@gmail.com>
2022-12-28 22:19:25 +0800

    Er'

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,29 +2,54 @@ import express from "express";
 import cors from "cors";
 import db from "./db";
 import routes from "./routes";
+import path from "path";
+
 const app = express();
 const port = process.env.PORT || 4000;
-
+const __dirname = path.resolve();
+console.log(__dirname);
 app.use(express.json());
 app.use(cors());
-
-db.connect();
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
 }
 
-app.get("/", (req, res) => {
+if (process.env.NODE_ENV === "development") {
+  console.log("Df");
+  app.use(cors());
+}
+app.use(express.json());
+app.use("/api", routes);
+db.connect();
+/*app.get("/", (req, res) => {
+  // send the request back to the client
+  //  console.log("GET /api");
+  res.send({ message: "Hello from the server!" }).status(200);
+  const __dirname = path.resolve();
+  console.log(__dirname);
+
+  //app.use(express.static(path.join(__dirname, "../../frontend", "build")));
+  /*app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../frontend", "build", "index.html"));
+  });
+});*/
+
+/*app.get("/api", (req, res) => {
   // send the request back to the client
   console.log("GET /api");
   res.send({ message: "Hello from the server!" }).status(200);
 });
 app.use("/api", routes);
 if (process.env.NODE_ENV === "production") {
+  console.log("dhfisfuwhei");
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("/", function (req, res) {
+  app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
   });
 }
-
+*/
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_ROOT =
   process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/api";
 
-export const instance = axios.create({ baseURL: API_ROOT });
+const instance = axios.create({ baseURL: API_ROOT });
 
 export default instance;
 
